595c7f6e684023aa1f856feb92e7eabf90c74b06
Guilherme Thomazi <thomazi@linux.com>
2016-11-15 01:07:10 -0200

    adding new commands

--- bucket.py
+++ bucket.py
@@ -1,37 +1,84 @@
+#!/usr/bin/python
+import boto3
+import os
+from botocore.exceptions import ClientError
 from cmd import Cmd
 
+
 class BucketPrompt(Cmd):
+    s3 = boto3.resource('s3')
+    bucket = None
+    bucket_name = None
 
     def do_use(self, args):
         """Changes your current bucket."""
-        if len(args) == 0:
-            print("I need a bucket name!")
-        else:
+        try:
+            assert len(args) > 0
             try:
                 self.bucket_name = args
+                self.bucket = self.s3.Bucket(self.bucket_name)
+                assert self.bucket in self.s3.buckets.all()
                 print("Using bucket %s" % self.bucket_name)
-            except:
+            except AssertionError:
                 print("Invalid bucket!")
+        except AssertionError:
+            print("I need a bucket name!")
 
     def do_pwd(self, args):
         """Shows which bucket you are using."""
         try:
+            assert len(args) == 0
             print('You are using the following bucket: %s' % self.bucket_name)
-        except AttributeError:
-            print("Undefined bucket!")
-    
+        except AssertionError:
+            print('This command takes no arguments!')
+
     def do_up(self, args):
         """Uploads a file to your current bucket."""
+        try:
+            assert len(args) > 0
+            basename = os.path.basename(args)
+            with open(args, 'rb') as f:
+                self.s3.Object(self.bucket_name, basename).put(Body=f)
+                print('%s uploaded!' % basename)
+        except AssertionError:
+            print("I need a file name!")
 
-    
+    def do_del(self, args):
+        """Deletes a file from your current bucket."""
+        try:
+            assert len(args) > 0
+            file_name = args
+            try:
+                self.s3.Object(self.bucket_name, file_name).load()
+                self.s3.Object(self.bucket_name, file_name).delete()
+                print('File %s deleted!' % file_name)
+            except ClientError:
+                print('File %s not found in bucket %s' % (file_name, self.bucket_name))
+        except AssertionError:
+            print("I need a file name!")
+
     def do_ls(self, args):
         """List files and folders in your current bucket."""
-    
-    
-    def do_quit(self, args):
+        try:
+            assert len(args) == 0
+            try:
+                assert self.bucket is not None
+                for obj in self.bucket.objects.all():
+                    print(obj.bucket_name, obj.key)
+            except AssertionError:
+                print('I need a bucket!')
+        except AssertionError:
+            print('This command takes no arguments!')
+
+    @staticmethod
+    def do_quit(args):
         """Quits the program."""
-        print("¯\_(ツ)_/¯")
-        raise SystemExit
+        try:
+            assert len(args) == 0
+            print("¯\_(ツ)_/¯")
+            raise SystemExit
+        except AssertionError:
+            print('This command takes no arguments!')
 
 
 if __name__ == '__main__':
---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='bucket',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/guitmz/bucket',
+    license='Apache License, Version 2.0',
+    author='Guilherme Thomazi Bonicontro',
+    author_email='thomazi@linux.com',
+    description='CLI tool to manage S3 files',
+    install_requires=[
+        'boto3'
+    ]
+)
