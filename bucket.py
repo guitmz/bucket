5dd74a40f1eec1ebd6391d41750b3b7155064c7b
Guilherme Thomazi <thomazi@linux.com>
2016-11-15 02:26:04 -0200

    adding option to compress file

--- bucket.py
+++ bucket.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 import boto3
+import gzip
 import os
 from botocore.exceptions import ClientError
 from cmd import Cmd
+from io import BytesIO
 
 
 class BucketPrompt(Cmd):
@@ -36,12 +38,34 @@ class BucketPrompt(Cmd):
         """Uploads a file to your current bucket."""
         try:
             assert len(args) > 0
-            basename = os.path.basename(args)
-            with open(args, 'rb') as f:
-                self.s3.Object(self.bucket_name, basename).put(Body=f)
-                print('%s uploaded!' % basename)
+            path = args
+            compress = self.should_compress_file()
+            self.upload_file(path, compress)
         except AssertionError:
             print("I need a file name!")
+
+    @staticmethod
+    def gzip_stream(file):
+        gz_stream = BytesIO()
+        with gzip.GzipFile(fileobj=gz_stream, mode='wb') as gzf:
+            gzf.write(file.read())
+        gz_stream.seek(0)
+        return gz_stream
+
+    def upload_file(self, path, compress):
+        basename = os.path.basename(path)
+        try:
+            with open(path, 'rb') as f:
+                if compress:
+                    gz_file = self.gzip_stream(f)
+                    compressed_file = basename + '.gz'
+                    self.s3.Object(self.bucket_name, compressed_file).put(Body=gz_file)
+                    print('%s uploaded!' % compressed_file)
+                else:
+                    self.s3.Object(self.bucket_name, basename).put(Body=f)
+                    print('%s uploaded!' % basename)
+        except FileNotFoundError:
+            print('File %s not found!' % path)
 
     def do_del(self, args):
         """Deletes a file from your current bucket."""
@@ -79,6 +103,15 @@ class BucketPrompt(Cmd):
             raise SystemExit
         except AssertionError:
             print('This command takes no arguments!')
+
+    @staticmethod
+    def should_compress_file():
+        while True:
+            user_input = input('Compress file? [y/n]: ')
+            if user_input in ['y', 'Y']:
+                return True
+            elif user_input in ['n', 'N']:
+                return False
 
 
 if __name__ == '__main__':
